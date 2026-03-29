e4ba7cbbce4786e0968adf134d0c6414a648660c
Luis Fernando Bianconi <luis.bianconi@gmail.com>
2026-03-29 12:58:56 +0100

    chore: updating files

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 # Creating the CloudSpace
 resource "spot_cloudspace" "kubernetes_cloudspace" {
   cloudspace_name    = "bianconi-dev"         ## Set The name of your Cloud Space here
-  region             = "us-east-iad-1"    ## Set region of your Cloud Space here, see the README.md for the options
+  region             = "us-central-dfw-1"    ## Set region of your Cloud Space here, see the README.md for the options
   hacontrol_plane    = false              ## DON'T ENABLE THIS, YOU NEED TO PAY MORE!!!, ONLY ENABLE IF YOU WANT THE H.A FOR CONTROL PLANE!!!
   cni                = "cilium"          # Choose te CNI "calico" "cilium" or "byocni" (byocni bring you own CNI)
-  kubernetes_version = "1.31.1"           # Kubernetes Version supported 1.29.6 | 1.30.10 | 1.31.1
+  kubernetes_version = "1.33.0"           # Kubernetes Version supported 1.33.0, 1.32.9, 1.31.1, 1.30.10, 1.29.6
   preemption_webhook = var.preemption_webhook
   wait_until_ready   = true
 }
@@ -12,9 +12,9 @@ resource "spot_cloudspace" "kubernetes_cloudspace" {
 # Creating the Spot Nodepool
 resource "spot_spotnodepool" "kubernetes_nodepool" {
   cloudspace_name = resource.spot_cloudspace.kubernetes_cloudspace.cloudspace_name
-  server_class    = "gp.vs1.medium-iad"     ## Server basic type, 2 cores and 4GB of RAM per node.
+  server_class    = "gp.vs1.medium-dfw"     ## Server basic type, 2 cores and 4GB of RAM per node.
   bid_price       = 0.001                   ## Fixed bid price to pay only 0.001/hour per node instance!
-  #desired_server_count = 2   ## Number of nodes that you want - Disabling to enable autoscaling - READ https://spot.rackspace.com/docs/en/autoscaling-a-spot-node-pool
+  #desired_server_count = 0   ## Number of nodes that you want - Disabling to enable autoscaling - READ https://spot.rackspace.com/docs/en/autoscaling-a-spot-node-pool
 
 ## Enable Autoscaling with 1 node max 2 nodes
   autoscaling = {
@@ -22,11 +22,15 @@ resource "spot_spotnodepool" "kubernetes_nodepool" {
     min_nodes = 1
     max_nodes = 2
   }
+
+  labels = {
+    "managed-by" = "terraform"
+    "owner" = "luisbianconi"
+  }
 }
 
-# Getting the kubeconfig file
-# To see RUN: "tofu/terraform output kubeconfig" command
-# If Fails, you can download on your rackspace the kubeconfig file!
-data "spot_kubeconfig" "kubernetes_kubeconfig" {
-  cloudspace_name = resource.spot_cloudspace.kubernetes_cloudspace.cloudspace_name
+# Writing the kubeconfig file
+resource "local_file" "kubeconfig" {
+  content  = data.spot_kubeconfig.kubernetes_kubeconfig.raw
+  filename = pathexpand("~/.kube/config")
 }
--- provider.tf
+++ provider.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     spot = {
       source = "rackerlabs/spot"
+      version = "0.1.4"
     }
   }
 }
